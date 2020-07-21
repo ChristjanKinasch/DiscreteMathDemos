afd02b8e9c4184dfd302e7907b15c533310b800d
Christjan Kinasch <c.kinasch@gmail.com>
2020-07-21 16:57:45 +1000

    altered param names, using lambda for LCM to remove print statements

--- Core/NumberTheory/1_primes_euclidean.py
+++ Core/NumberTheory/1_primes_euclidean.py
@@ -1,5 +1,4 @@
 # Gather prime factors, return as array
-#   Note: Positive Integers
 def prime_factors(n):
     primes = []
     steps = []
@@ -8,7 +7,7 @@ def prime_factors(n):
     # Repeatedly check against smallest prime divisors starting at 2
     # increment until all are found
     while n >= p*p:
-        # ascertain divisibility by 'p'
+        # determine divisibility by 'p'
         if not n % p == 0:
             p+=1
         else:
@@ -27,28 +26,32 @@ def prime_factors(n):
     return primes, steps if (len(steps) > 0) else "%s is Prime" % n
 
 #Greatest Common Divisor with Euclidean Algorithm
-#   Note: Positive Integers for x < y
-def euclid_gcd(x, y):
+#   Note: Positive Integers for a < b
+def euclid_gcd(a, b):
     # Incremental steps
-    print("%s divides %s" %(y, x))
+    print("%s divides %s" %(b, a))
     # Recursively call self if next step does not = 0 else return GCD
-    return euclid_gcd(y%x, x) if (not y%x == 0) else x
+    return euclid_gcd(b%a, a) if (not b%a == 0) else a
+
+#   Functionally identical to the above but does not print incremental steps.
+#   written to experiment with lambda expressions
+euclid_gcd_b = lambda a, b : euclid_gcd_b(b%a, a) if (not b%a == 0) else a
 
 # uses return value of gcd to determine the LCM
+# uses the lambda form of euclid_gcd so there are no annoying print statements
 def lcm(a, b):
-    x = euclid_gcd(a, b)
+    x = euclid_gcd_b(a, b)
     print("LCM of %s and %s = %s" % (a, b, (a*b)//x))
 
 
-#   Functionally identical to the above function, does not print incremental steps. 
-#   written to experiment with lambda expressions
-# euclid_gcd = lambda x, y : euclid_gcd(y%x, x) if (not y%x == 0) else print("GCD = %s" % x)
+
 
 #Test Cases
 print("135: ", prime_factors(135))
 print("*"*30)
 print("75: ", prime_factors(75))
 print("*"*30)
+print("9145: ", prime_factors(9145))
 
 print("*"*30)
 print("GCD of 12, 140 = ",euclid_gcd(12, 140))
